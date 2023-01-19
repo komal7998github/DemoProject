3dfd3162814446dd7cd2aa47ca973af93ef001f5
Komal <komalshrirame2016@gmail.com>
2023-01-19 19:12:04 +0530

    Data Added

---
+++ src/com/gitproject/HashSetDemo.java
@@ -0,0 +1,15 @@
+package com.gitproject;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+	public static void main(String[] args) {
+		HashSet<String> hs=new HashSet<String>();
+		hs.add("Komal");
+		hs.add("Kalpanna");
+		hs.add("Kamini");
+		hs.add("Kanchan");
+	System.out.println(hs);
+	}
+
+}
